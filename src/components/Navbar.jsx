d9d98db8464bfe919c942102bd6b9d9ae9966243
Tania Olarte Avila <taniaolarte@yahoo.com>
2026-05-18 17:01:46 +1000

    Version control

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
     <nav>
       <a href="#" className="nav-logo" onClick={(e) => e.preventDefault()}>
         TANIA OLARTE<span>.</span>
-        <span className="version-badge">v1.3.73</span>
+        <span className="version-badge">v1.4.2</span>
       </a>
 
       <div className={`nav-links${open ? ' open' : ''}`}>
